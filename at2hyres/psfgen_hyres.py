cb640efb59d4b47ecac059e98087e0582805d12f
Shanlong Li <shanlongli@umass.edu>
2023-09-27 17:27:23 -0400

    Add files via upload

--- at2hyres/psfgen_hyres.py
+++ at2hyres/psfgen_hyres.py
@@ -6,6 +6,6 @@ inp = sys.argv[1]
 out = sys.argv[2]
 
 gen = PsfGen()
-gen.read_topology('top_hyres2.inp')
+gen.read_topology('top_hyres_GPU.inp')
 gen.add_segment(segid='A', pdbfile=inp)
 gen.write_psf(filename=out)
