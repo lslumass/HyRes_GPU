dd3552cce7b70bd456ea150d036c89d96cb3cf6a
Shanlong Li <shanlongli@umass.edu>
2023-10-01 02:44:40 -0400

    Add files via upload

--- at2hyres/peptide_build.py
+++ at2hyres/peptide_build.py
@@ -1,30 +1,31 @@
+import sys
+from PeptideBuilder import Geometry
+import PeptideBuilder
+import Bio.PDB
+
 """
 Simple example script demonstrating how to use the PeptideBuilder library.
 Usage: python peptide_build.py seq_file output_file_name
 """
-import sys
-from PeptideBuilder import Geometry
-import PeptideBuilder
 
 seq_file = sys.argv[1]
 out_file = sys.argv[2]
 
-geo = Geometry.geometry("R")
+with open(seq_file, 'r') as f:
+    sequence = f.readlines()[0]
+    sequence = sequence.strip()
+
+geo = Geometry.geometry(sequence[0])
 #geo.phi = 0
 #geo.psi_im1 = 0
 structure = PeptideBuilder.initialize_res(geo)
 
-with open(seq_file, 'r') as f:
-    sequence = f.readlines()[0]
-for seq in sequence:
+for seq in sequence[1:]:
     geo = Geometry.geometry(seq)
     PeptideBuilder.add_residue(structure, geo)
 # add terminal oxygen (OXT)
 #eptideBuilder.add_terminal_OXT(structure)
 
-import Bio.PDB
-
 out = Bio.PDB.PDBIO()
 out.set_structure(structure)
 out.save(out_file)
-
