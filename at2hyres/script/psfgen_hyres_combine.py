8cee8bf5f93495580f833f8afca36584e5acd1ad
jianhuang <55832564+huangjianhuster@users.noreply.github.com>
2024-03-22 12:48:28 -0400

    Update psfgen_hyres_combine.py
    fix bugs made by shanlong

--- at2hyres/script/psfgen_hyres_combine.py
+++ at2hyres/script/psfgen_hyres_combine.py
@@ -16,6 +16,6 @@ num = len(sys.argv)
 for i in range(2, num):
     if sys.argv[i] != '':
         segid = 'P'+str(i)
-        gen.add_segment(segid=segid, pdbfile=sys.argv[i])
+        gen.add_segment(segid=segid, pdbfile=sys.argv[i], auto_angles=False, auto_dihedrals=False)
 
 gen.write_psf(filename=out)
