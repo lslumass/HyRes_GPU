546f7855cddd285697206c1f5a6d047833156350
lslumass <shanlongli@umass.edu>
2024-11-14 10:17:31 -0500

    update

--- run_OpenMM/run.py
+++ run_OpenMM/run.py
@@ -20,7 +20,7 @@ parser.add_argument('-p', "--psf", default='conf.psf', help="psf file, default i
 parser.add_argument('-t', "--temp", default=303, type=float, help="system temperature, default is 303 K")
 parser.add_argument('-b', "--box", nargs='+', type=float, help="box dimensions in nanometer, e.g., '50 50 50' ")
 parser.add_argument('-s', "--salt", default=150.0, type=float, help="salt concentration in mM, default is 150 mM")
-parser.add_argument('-e', "--ens", default='NVT', type=str, help="simulation ensemble, NPT or NVT")
+parser.add_argument('-e', "--ens", default='NVT', type=str, help="simulation ensemble, NPT, NVT, or non, non is for non-periodic system")
 
 args = parser.parse_args()
 pdb_file = args.pdb
@@ -28,22 +28,25 @@ psf_file = args.psf
 T = args.temp
 c_ion = args.salt/1000.0                                   # concentration of ions in M
 ensemble = args.ens
-if ensemble not in ['NPT', 'NVT']:
-    print("Error: The ensemble must be NPT or NVT. The input value is {}.".format(ensemble))
+
+## set pbc and box vector
+if ensemble in ['NPT', 'NVT']:
+    # pbc box length
+    if len(args.box) == 1:
+        lx, ly, lz = args.box[0], args.box[0], args.box[0]
+    elif len(args.box) == 3:
+        lx = args.box[0]*unit.nanometer
+        ly = args.box[1]*unit.nanometer
+        lz = args.box[2]*unit.nanometer
+    else:
+        print("Error: You must provide either one or three values for box.")
+        exit(1)
+    a = Vec3(lx, 0.0, 0.0)
+    b = Vec3(0.0, ly, 0.0)
+    c = Vec3(0.0, 0.0, lz)
+elif ensemble not in ['NPT', 'NVT', 'non']:
+    print("Error: The ensemble must be NPT, NVT or non. The input value is {}.".format(ensemble))
     exit(1)
-# pbc box length
-if len(args.box) == 1:
-    lx, ly, lz = args.box[0], args.box[0], args.box[0]
-elif len(args.box) == 3:
-    lx = args.box[0]*unit.nanometer
-    ly = args.box[1]*unit.nanometer
-    lz = args.box[2]*unit.nanometer
-else:
-    print("Error: You must provide either one or three values for box.")
-    exit(1)
-a = Vec3(lx, 0.0, 0.0)
-b = Vec3(0.0, ly, 0.0)
-c = Vec3(0.0, 0.0, lz)
 
 # simulation parameters
 dt = 0.0001*unit.picoseconds		                               # equilibration time step, production time step is 0.004
@@ -72,12 +75,16 @@ ffs = {
 print('\n################## load coordinates, topology and parameters ###################')
 pdb = PDBFile(pdb_file)
 psf = CharmmPsfFile(psf_file)
-psf.setBox(lx, lx, lx)
 top = psf.topology
-params = CharmmParameterSet(*top_inp, *param_inp)
-system = psf.createSystem(params, nonbondedMethod=CutoffPeriodic, constraints=HBonds)
+params = CharmmParameterSet(top_inp, param_inp)
+if ensemble == 'non':
+    system = psf.createSystem(params, nonbondedMethod=CutoffNonPeriodic, constraints=HBonds)
+else:
+    system = psf.createSystem(params, nonbondedMethod=CutoffPeriodic, constraints=HBonds)
+    psf.setBox(lx, ly, lz)
+    system.setDefaultPeriodicBoxVectors(a, b, c)
+
 system = HyresFF.HyresProteinSystem(psf, system, ffs)
-system.setDefaultPeriodicBoxVectors(a, b, c)
 
 with open('system.xml', 'w') as output:
     output.write(XmlSerializer.serialize(system))
@@ -87,18 +94,21 @@ for force in system.getForces():
     print('      ', force.getName())
 
 # simulation
+print('\n################### prepare simulation system####################')
 if ensemble == 'NPT':
-    print('\n################### prepare simulation system with NPT ####################')
+    print('This is a NPT system')
     system.addForce(MonteCarloBarostat(pressure, temperture, 25))
 elif ensemble == 'NVT':
-    print('\n################### prepare simulation system with NVT ####################')
+    print('This is a NVT system')
+elif ensemble == 'non':
+    print('This is a non-periodic system')
 integrator = LangevinMiddleIntegrator(temperture, friction, dt)
 plat = Platform.getPlatformByName('CUDA')
 prop = {'Precision': 'mixed', 'DeviceIndex': gpu_id}
 simulation = Simulation(top, system, integrator, plat, prop)
 simulation.context.setPositions(pdb.positions)
 simulation.context.setVelocitiesToTemperature(temperture)
-print('Langevin, CUDA, {}'.format(temperture))
+print(f'Langevin, CUDA, {temperture}')
 
 print('\n################### Minimization, Equilibriation, Production simulation ####################')
 print('# minimizeEnergy:')
@@ -106,10 +116,7 @@ print('before: ', simulation.context.getState(getEnergy=True).getPotentialEnergy
 simulation.minimizeEnergy(maxIterations=500000, tolerance=0.01)
 print('after: ', simulation.context.getState(getEnergy=True).getPotentialEnergy())
 
-print('\n# Equilibriation running loop1:')
-simulation.step(equil_step)
-print('\nloop2, dt=0.001')
-simulation.integrator.setStepSize(0.001*unit.picoseconds)
+print('\n# Equilibriation running:')
 simulation.step(equil_step)
 
 ## save a pdb traj using large step, dcd traj using small step, and log file
@@ -119,7 +126,7 @@ simulation.reporters.append(StateDataReporter('system.log', log_freq, progress=T
 #simulation.reporters.append(CheckpointReporter('system.chk', dcd_freq*10))
 
 simulation.integrator.setStepSize(0.004*unit.picoseconds)
-print('\n# NVT simulation running:')
+print('\n# Production simulation running:')
 simulation.step(total_step)
 
 simulation.saveCheckpoint('system.chk')
